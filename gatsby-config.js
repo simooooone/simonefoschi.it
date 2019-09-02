069dbb70f0db2f248434c8d1d9e45b054d0f306c
simooooone <s.foschi@protonmail.com>
2019-09-02 08:30:12 +0200

    deleted purgecss

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,7 @@ module.exports = {
         path: `${__dirname}/src/`,
       },
     },
-      {
+    /*  {
        resolve: 'gatsby-plugin-purgecss',
        options: {
          printRejected: true, // Print removed selectors and processed file names
@@ -24,7 +24,7 @@ module.exports = {
          // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
          // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
       }
-    },
+    },*/
     'gatsby-plugin-sharp',
     {
       resolve: 'gatsby-transformer-remark',

9b6b151e44e3e9711f4377b649e9d754d3c41364
simooooone <s.foschi@protonmail.com>
2020-04-01 14:24:13 +0200

    Added SEO description to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,18 +1,22 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React from "react"
+import { Link } from "gatsby"
 
-import Layout from '../components/layout'
-import Head from '../components/head'
+import Layout from "../components/layout"
+import Head from "../components/head"
 
 const NotFound = () => {
-    return (
-        <Layout myimg="1">
-
-            <Head  title="404 | Page not found" description="" />
-            <h1>Page not found :-(</h1>
-            <p><Link to="/">Head home</Link></p>
-        </Layout>
-    )
+  return (
+    <Layout myimg="1">
+      <Head
+        title="404 | Page not found"
+        description="Sorry, the page you requested cannot be found."
+      />
+      <h1>Page not found :-(</h1>
+      <p>
+        <Link to="/">Head home</Link>
+      </p>
+    </Layout>
+  )
 }
 
 export default NotFound

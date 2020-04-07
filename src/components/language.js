a3193602ccfb97a5a4d142bba1d4231371005b52
simooooone <s.foschi@protonmail.com>
2020-04-07 21:25:07 +0200

    Configuring language selection

--- src/components/language.js
+++ src/components/language.js
@@ -1,12 +1,17 @@
-import react from 'react'
+import React from "react"
+//const path = require("path")
 
 const Language = () => {
-    return (
-        <>
-            <a href="/">ITA</a>
-            <a href="/">ENG</a>
-        </>
-    )
+  //console.log(path.path(node.fileAbsolutePath))
+  /* if (path.path(node.fileAbsolutePath).dir == 'en/') {
+      
+  } */
+  return (
+    <>
+      <a href="/">ITA</a>
+      <a href="/">ENG</a>
+    </>
+  )
 }
 
 export default Language

15b485bf0a3046bcdfc0b577533ab236fcab6911
Simone Foschi <s.foschi@protonmail.com>
2021-04-11 22:05:43 +0200

    Added Web Vitals

--- gatsby-config.js
+++ gatsby-config.js
@@ -177,6 +177,22 @@ module.exports = {
           },
         ],
       },
+    }, // gatsby-config.js
+    {
+      resolve: "gatsby-plugin-web-vitals",
+      options: {
+        // The Google Analytics property ID; the reporting code won't be generated without it
+        trackingId: "YOUR_GOOGLE_ANALYTICS_TRACKING_ID",
+        // An array with metrics you want to track and send to analytics
+        metrics: [`FID`, `TTFB`, `LCP`, `CLS`, `FCP`],
+        // Event Category (optional) { string }, default 'Web Vitals'
+        eventCategory: "Performance",
+        // Include Web Vitals tracking in development
+        // Defaults to false meaning Vitals will only be tracked in production.
+        includeInDevelopment: false,
+        // Prints metrics in the console when true
+        debug: false,
+      },
     },
   ],
 }
